a0949306e4707a712073448875dc20e1dba7b40b
Alex Jienexa <alexjienexa@gmail.com>
2023-05-31 22:23:50 +0300

    Объявление класса Matrix

---
+++ headers/Matrix.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+/* Объявление класса */
+
+class Matrix {
+private:
+    std::vector<std::vector<double>> _data;
+    int _lenght;
+    int _weight;
+    size_t _size;
+
+public:
+
+}
