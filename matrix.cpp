27fe875f399a446ef4dbb2ff43ae1e370bf4bd99
Alex Jienexa <alexjienexa@gmail.com>
2023-05-31 21:35:52 +0300

    Создать первичный конструктор класса

--- matrix.cpp
+++ matrix.cpp
@@ -4,7 +4,20 @@
 #include <vector>
 
 class Matrix {
+private:
+    std::vector<std::vector<double>> data; // Данные матрицы
+public:
+    const int width; // ширина матрицы
+    const int height; // высота матрицы
 
+    /* Конструкторы */
+
+    Matrix(int w, int h) : width(w), height(h) {
+        data.resize(height);
+        for (auto elem : data) {
+            elem.resize(width);
+        }
+    }
 };
 
 
