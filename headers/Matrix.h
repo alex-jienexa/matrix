c5a9f465ac865080b0a880ac74cc8beda1095f36
Alex Jienexa <alexjienexa@gmail.com>
2023-05-31 22:29:47 +0300

    Объявление класса Matrix 2

--- headers/Matrix.h
+++ headers/Matrix.h
@@ -1,14 +1,25 @@
 #include <vector>
+#include <string>
 
 /* Объявление класса */
 
 class Matrix {
 private:
-    std::vector<std::vector<double>> _data;
-    int _lenght;
-    int _weight;
-    size_t _size;
+    std::vector<std::vector<double>> _data; // Данные матрицы  
 
+    int _lenght;    // Длина матрицы, => len(data[n])
+
+    int _height;    // Высота матрицы, => len(data)
 public:
+    Matrix();                       // По умолчанию
 
-}
+    Matrix(const Matrix& other);    // Копирование другой матрицей
+    
+    /* Задание элементов разными типами */
+    
+    Matrix(const std::vector<std::vector<double>>); 
+
+    Matrix(const std::vector<std::vector<int>>);
+
+    Matrix(const std::vector<std::vector<std::string>>); // Предполагается использование std::stod(...)
+};
