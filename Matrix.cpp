6c815d42b3225fc1b9d399337c827146b78057bf
Alex Jienexa <alexjienexa@gmail.com>
2023-05-31 23:04:10 +0300

    Расписывание будущих планов
    Все будущие планы написаны в конце файла headers/Matrix.h, вскоре будут
    удалены и/или перенесены в другой файл

---
+++ Matrix.cpp
@@ -0,0 +1,17 @@
+#include "Matrix.h"
+
+Matrix::Matrix() {
+    _data = {};
+}
+
+//TODO: Закончить объявление матрицы
+
+// Matrix::Matrix(const Matrix& other);    // Копирование другой матрицей
+    
+/* Задание элементов разными типами */
+
+// Matrix(const std::vector<std::vector<double>>); 
+
+// Matrix(const std::vector<std::vector<int>>);
+
+// Matrix(const std::vector<std::vector<std::string>>); // Предполагается использование std::stod(...)
