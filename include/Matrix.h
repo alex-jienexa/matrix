5c2b3c4238de870d9dae3a8eb43795b0bd47e0f1
Alex Jienexa <alexjienexa@gmail.com>
2023-06-05 21:40:36 +0300

    Фикс include/Matrix.h Некоторые функции теперь не принимают (const Matrix&) ввиду особенности их работы с текущим объектом класса.

--- include/Matrix.h
+++ include/Matrix.h
@@ -38,14 +38,14 @@ public:
     // Создание особых матриц / Creation of special matrix
         // Будут объявлены в ./constructors отдельным файлом
     Matrix identity() const; // Единичная матрица = элементы на главной диагонали единицы
-    Matrix transposed(const Matrix&) const; // Транспонированная матрица = смена строк и столбцов местами
+    Matrix transposed() const; // Транспонированная матрица = смена строк и столбцов местами
     Matrix square(const int&); // Квадратная матрица n*n
 
     // Особые операции с матрицами / Special operations with matrix
-    Matrix inverse(const Matrix&) const; // Обратная матрица A^-1
-    double det(const Matrix&) const; // Определитель матрицы
-    double trace(const Matrix&) const; // След матрицы = суммирование элементов главной диагонали матрицы
-    double minor(const Matrix&) const; // Минор матрицы
+    Matrix inverse() const; // Обратная матрица A^-1
+    double det() const; // Определитель матрицы
+    double trace() const; // След матрицы = суммирование элементов главной диагонали матрицы
+    double minor(int i, int j) const; // Минор матрицы
 
     /* 
     ПРИМЕЧАНИЯ: 
